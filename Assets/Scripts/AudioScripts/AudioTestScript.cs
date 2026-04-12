58e3fd40c50b3a602cf4cdd9d34becec2006be4f
LoganMatuska <117881267+LoganMatuska@users.noreply.github.com>
2026-04-12 15:36:38 -0400

    Successfully routed Mac and Windows Soundbank Folders
    After a little elbow grease, the soundbanks (the files that have all audio information) should safely be findable inside the Unity project.
    
    For now, sounds are kept as OGG Vorbis to save on space.

---
+++ Assets/Scripts/AudioScripts/AudioTestScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AudioTestScript : MonoBehaviour
+{
+    public AK.Wwise.Bank soundBank;
+    
+    void Awake()
+    {
+        soundBank.Load();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AkSoundEngine.PostEvent("StartGameAudio", gameObject);
+        AkSoundEngine.PostEvent("SetMusicToMainTheme", gameObject);
+    }
+}
